39d3b219ca7d478a3c00aa906601cb32e88dfb63
sechaser <413406236@qq.com>
2016-04-23 21:40:25 +0800

    sequential search, O(n)

--- main.cpp
+++ main.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
 
+#define N 10
+
 using namespace std;
+
+int sequentialSearch(int a[], int n, int target)
+{
+    for(int i = 0; i < n; ++ i)
+    {
+        if(a[i] == target)
+            return i;
+    }
+
+    return n;
+}
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int a[N] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int pos = sequentialSearch(a, N, 11);
+    std::cout<<pos<<std::endl;
+
     return 0;
 }
 
